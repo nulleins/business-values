368f237640af59b6c9e7fc635bef141472688878
sothach <phillips.roy@gmail.com>
2015-02-27 14:21:16 +0000

    Update IBANScheme.scala
    Refactored scheme factory creation to remove dependency on Typesafe config and open up to other configuration methods (e.g., from database)

--- src/main/scala/org/nulleins/values/model/IBANScheme.scala
+++ src/main/scala/org/nulleins/values/model/IBANScheme.scala
@@ -1,55 +1,67 @@
 package org.nulleins.values.model
 
-import com.typesafe.config._
-import scalaz.{Success, Failure, Validation}
+import scala.io.Source
 import scalaz.syntax.std.option._
+import scalaz.{Failure, Success, Validation}
 
-case class IBANScheme(countryCode: ISO3166, bankPic: String, branchPic: String, accountPic: String) {
+case class IBANScheme(countryCode: ISO3166, bankPic: String, branchPic: String, accountPic: String)
+    extends AccountNumber {
   val pattern = s"^([A-Z]{2})([0-9]{2})${map(bankPic)}${map(branchPic)}${map(accountPic)}$$".r
 
-  def matches(value: String) = pattern.findFirstIn(value).toSuccess(s"$value does not match pattern")
   def countryCode(value: String) = ISO3166(value take 2)
   def bankCode(value: String) = value match { case pattern(_, _, a, _*) => a }
   def branchCode(value: String) = value match { case pattern(_, _, _, b, _) => b }
   def accountNumber(value: String) = value match { case pattern(_, _, _, _, c) => c }
 
-  lazy val length = Seq(bankPic, branchPic, accountPic).foldLeft(4)((acc, part) => {
-    part match { case specPattern(n, _) => acc + n.toInt; case _ => 0} })
-
-  private lazy val specPattern = "(\\d+)([anc])".r
-  private lazy val typeMap = Map("a" -> "A-Za-z", "n" -> "\\d", "c" -> "A-Za-z\\d")
-  private def map(spec: String) = {
-    spec match { case specPattern(n, t) => s"([${typeMap(t)}]{$n})"; case _ => ""}}
-}
-
-object IBANScheme extends AccountNumber {
-  import scala.collection.JavaConversions._
-
-  /* load country IBAN scheme definitions from configuration file on class path */
-  private lazy val configRoot = ConfigFactory.load("iban-schemes").getObject("iban-schemes")
-  private lazy val schemes = configRoot.unwrapped.toMap.map {
-    case (k: String, v: java.util.Map[String, String]) => val key = ISO3166(k)
-      key -> IBANScheme(key, v.getOrElse("bank", "0n"), v.getOrElse("branch", "0n"), v.getOrElse("account", "0n"))
-  }
-  def schemeFor(country: ISO3166) = schemes.get(country).toSuccess(s"Scheme not registered for $country")
-
-  /** @return `Success` wrapping the normalized input string, if: it has a scheme defined, it
-    *         matches the scheme pattern and the checksum is valid; else a `Failure` wrapping
+  /** @return the input `value`, normalized and wrapped in `Success` if: it has a scheme defined
+    *         it matches the scheme pattern and the checksum is valid; else a `Failure` wrapping
     *         the failure message detailing the (first) reason it was not considered valid */
-  def parse(value: String) = for {
-    normal <- normalize(value,5)
-    scheme <- schemeFor(ISO3166(normal take 2))
-    result <- scheme.matches(normal)
-    _ <- validateChecksum(normal)
+  def parse(value: String): Validation[String, String] = for {
+    normal <- normalize(value, 5)
+    result <- pattern.findFirstIn(normal).toSuccess(s"$value does not match pattern")
+    _ <- IBANScheme.validateChecksum(normal)
   } yield result
 
   def valid(value: String) = parse(value).isSuccess
 
-  def create(value: String): IBAN = IBAN(value, schemes(ISO3166(value take 2)))
+  def create(value: String): IBAN = parse(value) fold(
+    failure => throw new RuntimeException(failure),
+    success => IBAN(success, this))
 
-  def validateChecksum(iban: String) = if ( checksumValid(iban)) Success(iban) else Failure("Invalid checksum")
+  lazy val length = Seq(bankPic, branchPic, accountPic).foldLeft(4)((acc, part) => {
+    part match { case specPattern(n, _) => acc + n.toInt; case _ => 0} })
+
+  private lazy val specPattern = "(\\d+)([AaNnCc])".r
+  private lazy val typeMap = Map("A" -> "A-Za-z", "N" -> "\\d", "C" -> "A-Za-z\\d")
+  private def map(spec: String) = {
+    spec match { case specPattern(n, t) => s"([${typeMap(t.toUpperCase)}]{$n})"; case _ => ""} }
+}
+
+object IBANScheme {
+  val configPattern = """\s*([A-Z]{2})\s*[=:]\s*(\d+[ANC])\s*(\d+[ANC])\s*(\d+[ANC])\s*""".r
+  def apply(spec: String): IBANScheme = spec.toUpperCase match {
+    case configPattern(cc, bank, branch, account) => IBANScheme(ISO3166(cc), bank, branch, account)
+    case _ => throw new RuntimeException(s""""$spec"" is not a valid scheme specification""")
+  }
+  def validateChecksum(code: String) = if (checksumValid(code)) Success(code) else Failure("Invalid checksum")
   def generateChecksum(cc: ISO3166, bban: String) = 98 - mod97of(s"${cc}00$bban")
-  def checksumValid(iban: String) = mod97of(iban) == BigInt(1)
-  private def mod97of(iban: String) = BigInt(((iban drop 4) + (iban take 4)).map { cc =>
+  def checksumValid(code: String) = mod97of(code) == BigInt(1)
+  private def mod97of(code: String) = BigInt(((code drop 4) + (code take 4)).map { cc =>
     if (cc isDigit) cc.toInt - '0' else cc - 'A' + 10 }.mkString) mod 97
+}
+
+class IBANFactory(schemes: Map[ISO3166,IBANScheme]) {
+  implicit def getISO3166(value : String): ISO3166 = ISO3166(if (value != null) value.trim take 2 else "")
+  def schemeFor(country: ISO3166) = schemes.get(country).toSuccess(s"""Scheme not registered for "$country"""")
+  def create(value: String): IBAN = schemeFor(value) fold (
+    failure => throw new RuntimeException(failure),
+    success => success.create(value))
+  def valid(value: String) = schemeFor(value) fold (failure => false, success => success.valid(value))
+}
+
+object IBANFactory {
+  def apply(values: IBANScheme*): IBANFactory = new IBANFactory(values.map(v => v.countryCode -> v).toMap)
+  def apply(configFile: String): IBANFactory = IBANFactory(
+    (Source.fromInputStream(getClass.getResourceAsStream(configFile)).getLines()
+      .filter(!_.startsWith("#")) map(line => IBANScheme(line))).toSeq :_*)
 }
