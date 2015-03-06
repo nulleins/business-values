a7c2a01a1f07d1e7a863955b21d755af4f771f35
sothach <phillips.roy@gmail.com>
2015-03-06 10:21:06 +0000

    Update PrefixMap.scala

--- src/main/scala/org/nulleins/values/PrefixMap.scala
+++ src/main/scala/org/nulleins/values/PrefixMap.scala
@@ -1,9 +1,10 @@
 import scala.annotation.tailrec
 import scala.collection.mutable
 
-case class TrieMapNode(
-    nodes: mutable.Map[Char, TrieMapNode] = mutable.Map[Char, TrieMapNode]()) {
+private case class PrefixNode(
+                               nodes: mutable.Map[Char, PrefixNode] = mutable.Map[Char, PrefixNode]()) {
   var leaf = false
+  def this(char: Char, node: PrefixNode) = this(mutable.Map(char -> node))
 
   @tailrec final def prefix(input: String, pos: Int = 0, longest: Int = 0): Int =
     if (input.isEmpty || !nodes.contains(input(0))) {
@@ -12,36 +13,24 @@ case class TrieMapNode(
       nodes(input(0)).prefix(
         input drop 1, pos + 1, if (leaf) pos else longest)
     }
+
+  def insert(word: String) = word.foldLeft(this)((next,ch) => {
+    next.nodes.getOrElseUpdate(ch, new PrefixNode(ch, this))
+  }).leaf = true
 }
 
-class TrieMap {
-  val root = new TrieMapNode
-
-  def insertWord(word: String) = {
-    var next = root
-    word foreach { ch =>
-      val child = next.nodes
-      if (child.contains(ch)) {
-        next = child(ch)
-      } else {
-        val node = new TrieMapNode
-        child.put(ch, node)
-        next = node
-      }
-    }
-    next.leaf = true
-  }
-
+class PrefixTree {
+  private val root = new PrefixNode
+  private def insertWord(word: String) = root.insert(word)
   def insert(words: String*) = words foreach insertWord
-
-  def apply(input: String) = input take root.prefix(input)
+  def apply(input: String): String = input take root.prefix(input)
 }
 
-object TrieMap {
-  def apply(words: String*): TrieMap = {
-    val result = new TrieMap
+object PrefixTree {
+  def apply(words: String*): PrefixTree = {
+    val result = new PrefixTree
     result.insert(words :_*)
     result
   }
-  def apply(words: List[String]): TrieMap = apply(words :_*)
+  def apply(words: List[String]): PrefixTree = apply(words :_*)
 }
