86c18d26eca18e89fd7fb85dbb0bc587aa79b43f
sothach <phillips.roy@gmail.com>
2015-03-13 10:30:08 +0000

    Update prefix.scala

--- src/test/prefix.scala
+++ src/test/prefix.scala
@@ -2,7 +2,7 @@ In Scala:
 
     /** @return prefix string that all of `list` start with, or the empty
       *         string when `list` is empty, or there is no common prefix
-      *   ensure: result.isEmpty || list.forall(_.startsWith(result)) */
+      * ensure: result.isEmpty || list.forall(_.startsWith(result)) */
     def commonPrefix(list: String*) = list.foldLeft("")((_,_)=>
       (list.min zip list.max).takeWhile(v => v._1 == v._2).unzip._1.mkString)
 
